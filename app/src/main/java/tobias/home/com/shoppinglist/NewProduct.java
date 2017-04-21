29c6c26957b0302cba571ec251a324c20b5bf9f7
Tobias Rödebäck <tobias.rodeback@gmail.com>
2017-04-21 09:08:59 +0200

    Second commit

--- app/src/main/java/tobias/home/com/shoppinglist/NewProduct.java
+++ app/src/main/java/tobias/home/com/shoppinglist/NewProduct.java
@@ -1,19 +1,19 @@
 package tobias.home.com.shoppinglist;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class NewProduct extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
-
-    private Spinner spinner;
-    private static final String[]paths = {"kilograms", "liter", "pieces"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +26,22 @@ public class NewProduct extends AppCompatActivity implements AdapterView.OnItemS
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+                SharedPreferences sharedPref = NewProduct.this.getPreferences(Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+
+                EditText qtyextract = (EditText)findViewById(R.id.quantityinput);
+                String qty = qtyextract.getText().toString();
+
+                EditText productextract = (EditText)findViewById(R.id.productinput);
+                String product = productextract.getText().toString();
+
+                String listitem = qty + " " + unit + " " + product;
+
+//                editor.putString(getString(R.string.saved_high_score), listitem);
+//                editor.commit();
+
+                NewProduct.this.finish();
             }
         });
 
@@ -40,19 +54,25 @@ public class NewProduct extends AppCompatActivity implements AdapterView.OnItemS
         spinner.setOnItemSelectedListener(this);
     }
 
+    public String unit;
+
+
+    private Spinner spinner;
+    private static final String[]paths = {"pieces", "liter", "kilograms"};
+
 
 
     public void onItemSelected(AdapterView<?> parent, View v, int position, long id) {
 
         switch (position) {
             case 0:
-                // Whatever you want to happen when the first item gets selected
+                unit = "pcs";
                 break;
             case 1:
-                // Whatever you want to happen when the second item gets selected
+                unit = "l";
                 break;
             case 2:
-                // Whatever you want to happen when the thrid item gets selected
+                unit = "kg";
                 break;
 
         }
